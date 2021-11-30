fb9f47a57e1602cbc0572af725d084c7f03bc3af
= <marcos.esteban@viewnext.com>
2021-11-30 12:30:21 +0100

    Room implementada

--- app/src/main/java/com/example/mvvmreal/data/database/FacturaDao.java
+++ app/src/main/java/com/example/mvvmreal/data/database/FacturaDao.java
@@ -3,12 +3,14 @@ package com.example.mvvmreal.data.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.TypeConverter;
 
 import com.example.mvvmreal.data.model.RespuestaFactura;
 
 @Dao
 public interface FacturaDao {
 
+    @TypeConverter()
     @Query("select *,count(FacturaId) from Factura")
     RespuestaFactura getAll();
 
